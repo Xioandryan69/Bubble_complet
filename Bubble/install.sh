410238660ec88371813140384aadd4cf3c041eeb
Xioandryan69 <andriannyavo9@gmail.com>
2026-02-11 16:02:28 +0300

    ok

--- Bubble/install.sh
+++ Bubble/install.sh
@@ -1,6 +1,7 @@
-sudo cp Bubble_complet/Bubble/provision.sh /usr/local/bin/
-sudo cp Bubble_complet/VirtualHost/serveur/user.sh /usr/local/bin/
-sudo cp Bubble_complet/Samba/run.sh /usr/local/bin/samba_add.sh
+
+sudo cp Bubble/provision.sh /usr/local/bin/
+sudo cp VirtualHost/serveur/user.sh /usr/local/bin/
+sudo cp Samba/run.sh /usr/local/bin/samba_add.sh
 
 sudo chmod 750 /usr/local/bin/provision.sh
 sudo chmod 750 /usr/local/bin/user.sh
