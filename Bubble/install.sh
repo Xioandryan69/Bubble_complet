d07385dcc3a7fdcb2bf689ff035e8aa434f99c18
Andriantsoa94 <voaharyralambo@gmail.com>
2026-02-11 16:03:29 +0300

    update install script to use relative paths and ensure proper ownership and permissions

--- Bubble/install.sh
+++ Bubble/install.sh
@@ -1,11 +1,10 @@
-sudo cp Bubble_complet/Bubble/provision.sh /usr/local/bin/
-sudo cp Bubble_complet/VirtualHost/serveur/user.sh /usr/local/bin/
-sudo cp Bubble_complet/Samba/run.sh /usr/local/bin/samba_add.sh
+sudo cp Bubble/provision.sh /usr/local/bin/
+sudo cp VirtualHost/serveur/user.sh /usr/local/bin/
+sudo cp Samba/run.sh /usr/local/bin/samba_add.sh
 
 sudo chmod 750 /usr/local/bin/provision.sh
 sudo chmod 750 /usr/local/bin/user.sh
 sudo chmod 750 /usr/local/bin/samba_add.sh
 
-chown root:root /usr/local/bin/{provision.sh,user.sh,samba_add.sh}
-chmod 750 /usr/local/bin/{provision.sh,user.sh,samba_add.sh}
-
+sudo chown root:root /usr/local/bin/{provision.sh,user.sh,samba_add.sh}
+sudo chmod 750 /usr/local/bin/{provision.sh,user.sh,samba_add.sh}
